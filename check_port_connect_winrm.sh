9a4f4051b9b245cf90ef6eab59255b567410b443
Gabriel Silva <42187636+gsfcosta@users.noreply.github.com>
2022-08-26 16:50:08 -0300

    Update check_port_connect_winrm.sh

--- check_port_connect_winrm.sh
+++ check_port_connect_winrm.sh
@@ -1,1 +1,25 @@
-
+#!/bin/bash
+clear
+x=0
+y=0
+for ip in $(cat ips.txt); do #ARQUIVO COM IPS/DNS UM ABAIXO DO OUTRO
+        nc -zvw 3 $ip 5985 #TESTANDO PORTA 5985 (WINRM HTTP)
+        if (( $? == 0 )); then
+                echo "$ip = HTTP" >> /tmp/result_success.txt
+                echo "$ip = HTTP > SUCCESS"
+                let x=($x+1)
+        else
+			nc -zvw 3 $ip 5985 #TESTANDO PORTA 5986 (WINRM HTTPS)
+        	if (( $? == 0 )); then
+        	        echo "$ip = HTTPS" >> /tmp/result_success.txt
+        	        echo "$ip = HTTPS > SUCCESS"
+        	        let x=($x+1)
+        	else
+        	        echo "$ip" >> /tmp/result_failed.txt
+        	        echo "$ip > FAILED"
+        	        let y=($y+1)
+        	fi
+        fi
+        echo -e "\n-------------------------------------------------\n"
+done
+echo -e "\nSucesso: $x\nFailed: $y"
